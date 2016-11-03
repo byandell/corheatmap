7bea7367cdd766ccfa2c25dc73299df92ea734b0
Brian Yandell <byandell@wisc.edu>
2016-11-03 15:54:52 -0500

    added interact to lm_pval

--- R/lm_pval.R
+++ R/lm_pval.R
@@ -4,60 +4,59 @@
 #' @param form formula
 #' @param group column to group by (or not if \code{NULL})
 #' @param type type of p-values (one of c("drop1","anova"))
+#' @param inter test for interactions if \code{TRUE}
+#' @param digits number of significant digits (default \code{3})
 #'
 #' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
 #' @keywords utilities
 #'
 #' @export
-lm_pval <- function(object, form, group=NULL, type=c("drop1","anova")) {
+lm_pval <- function(object, form, group=NULL, type=c("drop1","anova"),
+                    inter = TRUE, digits = 3) {
   type <- match.arg(type)
-  switch(type,
-         drop1 = {
-           if(is.null(group)) {
-             drop1_pval_one(object, form)
-           } else {
-             object %>%
-               ## Do ANOVA by each group level.
-               group_by_(group) %>%
-               do(drop1_pval_one(., form))
-           }
-         },
-         anova = {
-           if(is.null(group)) {
-             anova_pval_one(object, form)
-           } else {
-             object %>%
-               ## Do ANOVA by each group level.
-               group_by_(group) %>%
-               do(anova_pval_one(., form))
-           }
-         })
+  digits <- as.integer(digits)
+  stopifnot(!is.na(digits) & digits > 0 & digits < 7)
+  
+  ## Set up interaction formula from form
+  form <- formula(form)
+  if(inter) {
+    form2 <- as.character(form)
+    form2 <- formula(paste0(form2[2], "~(", form2[3], ")^2"))
+  } else {
+    form2 <- NULL
+  }
+  
+  fit_fn <- ifelse(type == "drop1", drop1, anova)
+  if(is.null(group)) {
+    pval_one(object, fit_fn, form, form2, digits)
+  } else {
+    object %>%
+      ## Do ANOVA by each group level.
+      group_by_(group) %>%
+      do(pval_one(., fit_fn, form, form2, digits))
+  }
 }
-anova_pval_one <- function(object, form) {
+pval_one <- function(object, fit_fn, form, form2, digits) {
   fit <- lm(form, data=object)
   sfit <- summary(fit)
-  sigma <- sfit$sigma
-  tidy(anova(fit)) %>%
-    select(term, p.value) %>%
-    filter(!is.na(p.value)) %>%
-    mutate(term = make.names(term)) %>%
-    spread(term,p.value) %>%
-    mutate(overall = pval_overall(sfit),
-           SD = sigma)
-}
-drop1_pval_one <- function(object, form) {
-  fit <- lm(form, data=object)
-  sfit <- summary(fit)
-  sigma <- sfit$sigma
-  tidy(drop1(fit, fit, test = "F") %>% select(-AIC)) %>%
-    select(term, p.value) %>%
-    filter(!is.na(p.value)) %>%
-    mutate(term = make.names(term)) %>%
-    spread(term,p.value) %>%
-    mutate(overall = pval_overall(sfit),
-           SD = sigma)
-}
-pval_overall <- function(sfit) {
+  sigma <- signif(sfit$sigma, digits)
+  fit2 <- lm(form2, data=object)
+
   fstat <- sfit$fstatistic
-  pf(fstat[1L], fstat[2L], fstat[3L], lower.tail = FALSE)
+  pval_overall <- signif(pf(fstat[1L], fstat[2L], fstat[3L], 
+                            lower.tail = FALSE),
+                         digits)
+  pval_inter <- signif(anova(fit2, fit, test="F")[2,"Pr(>F)"],
+                       digits)
+  
+  ## Tidy one-line tbl_df of p-values and SD
+  tidy(fit_fn(fit, fit, test = "F") %>% select(-AIC)) %>%
+    select(term, p.value) %>%
+    filter(!is.na(p.value)) %>%
+    mutate(p.value = signif(p.value, digits)) %>%
+    mutate(term = make.names(term)) %>%
+    spread(term,p.value) %>%
+    mutate(overall = pval_overall,
+           interact = pval_inter,
+           SD = sigma)
 }
