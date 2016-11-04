0136270e338c4d62aa90a2ee2eedf1d8524c893c
Brian Yandell <byandell@wisc.edu>
2016-11-04 08:18:01 -0500

    switched lm_pval params to match lm; handling FALSE inter argument

--- R/lm_pval.R
+++ R/lm_pval.R
@@ -1,7 +1,7 @@
 #' anova p-values grouped by column
 #'
-#' @param object data.frame or tbl_df
 #' @param form formula
+#' @param object data.frame or tbl_df
 #' @param group column to group by (or not if \code{NULL})
 #' @param type type of p-values (one of c("drop1","anova"))
 #' @param inter test for interactions if \code{TRUE}
@@ -9,9 +9,12 @@
 #'
 #' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
 #' @keywords utilities
+#' 
+#' @examples
+#' lm_pval(mpg ~ wt + cyl, mtcars)
 #'
 #' @export
-lm_pval <- function(object, form, group=NULL, type=c("drop1","anova"),
+lm_pval <- function(form, object, group=NULL, type=c("drop1","anova"),
                     inter = TRUE, digits = 3) {
   type <- match.arg(type)
   digits <- as.integer(digits)
@@ -39,24 +42,28 @@ lm_pval <- function(object, form, group=NULL, type=c("drop1","anova"),
 pval_one <- function(object, fit_fn, form, form2, digits) {
   fit <- lm(form, data=object)
   sfit <- summary(fit)
-  sigma <- signif(sfit$sigma, digits)
-  fit2 <- lm(form2, data=object)
 
   fstat <- sfit$fstatistic
   pval_overall <- signif(pf(fstat[1L], fstat[2L], fstat[3L], 
                             lower.tail = FALSE),
                          digits)
-  pval_inter <- signif(anova(fit2, fit, test="F")[2,"Pr(>F)"],
-                       digits)
   
   ## Tidy one-line tbl_df of p-values and SD
-  tidy(fit_fn(fit, fit, test = "F") %>% select(-AIC)) %>%
+  out <- tidy(fit_fn(fit, fit, test = "F") %>% select(-AIC)) %>%
     select(term, p.value) %>%
     filter(!is.na(p.value)) %>%
     mutate(p.value = signif(p.value, digits)) %>%
     mutate(term = make.names(term)) %>%
     spread(term,p.value) %>%
-    mutate(overall = pval_overall,
-           interact = pval_inter,
-           SD = sigma)
+    mutate(overall = pval_overall)
+
+  ## P-value for extra interactions
+  if(!is.null(form2)) {
+    fit2 <- lm(form2, data=object)
+    out$interact <- signif(anova(fit2, fit, test="F")[2,"Pr(>F)"],
+                           digits)
+  }
+
+  out$SD <- signif(sfit$sigma, digits)
+  out
 }
